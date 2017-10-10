c85534bdd045682dec96a39782f00f4cd578cc62
= <=>
2017-10-10 10:57:01 -0600

    add home message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    You are logged into Forum 83 Forward!
                 </div>
             </div>
         </div>
